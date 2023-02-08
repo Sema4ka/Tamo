ab5d5b1cfd705770a67e5c591ac8f22dfc85bb97
Sema4ka <mr.vasilchenoko@mail.ru>
2023-02-08 18:51:40 +0300

    bowl fix

--- Assets/Scripts/Pet/Bawl.cs
+++ Assets/Scripts/Pet/Bawl.cs
@@ -10,46 +10,54 @@ public class Bawl : MonoBehaviour
 {
     [SerializeField] private Sprite[] stages;
     [SerializeField] private float eatingRadius;
-    [SerializeField] private float eatingTime = 2;
+    [SerializeField] private float eatingTime;
     [SerializeField, Range(0, 7)] private int currentStage;
+    [SerializeField] private float defaultTime = 2;
+
     
     private Pet _pet;
     private Rigidbody2D _petRb;
     private Transform _petTransform;
-    private float _defaultTime;
     private SpriteRenderer _spriteRenderer;
 
-    private void Start()
+    protected virtual void Start()
     {
         _pet = FindObjectOfType<Pet>();
         _petRb = _pet.GetComponent<Rigidbody2D>();
         _petTransform = _pet.GetComponent<Transform>();
-        _defaultTime = eatingTime;
+        defaultTime = eatingTime;
         _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
-        _petRb.AddForce(transform.position - _petTransform.position);
+        if(currentStage!=0)
+            _petRb.AddForce(transform.position - _petTransform.position);
         _spriteRenderer.sprite = stages[currentStage];
         Eat();
     }
 
     private void Eat()
     {
-        if (Physics2D.OverlapCircle(transform.position, eatingRadius, LayerMask.GetMask("Player")))
+        Collider2D obj = Physics2D.OverlapCircle(transform.position, eatingRadius, 
+            LayerMask.GetMask("Player"));
+        if (obj)
         {
             eatingTime -= Time.deltaTime;
         }
         else
         {
-            eatingTime = _defaultTime;
+            eatingTime = defaultTime;
         }
 
         if (eatingTime <= 0)
         { 
+            Pet pet = obj.gameObject.GetComponent<Pet>();
             currentStage = currentStage > 0 ? currentStage - 1: 0;
-            eatingTime = _defaultTime;
+            eatingTime = defaultTime;
+            
+            if(currentStage!=0)
+                pet.Eat();
         }
     }
     
--- Assets/Scripts/Pet/Pet.cs
+++ Assets/Scripts/Pet/Pet.cs
@@ -1,13 +1,51 @@
 using System;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
 public class Pet : Entity
 {
-    [SerializeField]
-    protected int jumpForce = 10;
+    [SerializeField] protected int jumpForce = 10;
+    
+    [SerializeField] private int saturation;
+    [SerializeField] private int maxSaturation = 100;
+    [SerializeField] private float hungerTime = 300;
+
+    protected override void Start()
+    {
+        base.Start();
+        StartCoroutine(GetHungryCorutine());
+    }
     public void Catch(Transform target)
     {
         MyRigitbody.AddForce((transform.position - target.position)*-jumpForce, ForceMode2D.Impulse);
+    }
+
+    public void Eat()
+    {
+        if(saturation + 5 < maxSaturation)
+        {
+            saturation += 5;
+            MyTransform.localScale += new Vector3(0.05f, 0.05f, 0);
+        }        
+    }
+    
+    private void GetHungry()
+    {
+        if(saturation - 5 > 0)
+        {
+            saturation -= 5;
+            MyTransform.localScale -= new Vector3(0.05f, 0.05f, 0);
+        }        
+    }
+
+    IEnumerator GetHungryCorutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(hungerTime);
+            GetHungry();
+        }
     }
 }
